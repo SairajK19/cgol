daa86bc91784a9a73b2c37e5e69d36d2c014609f
SairajK19 <saikapdi11@gmail.com>
2024-06-29 23:55:05 +0530

    Small optimization - Only update cells that are changed

--- src/main.c
+++ src/main.c
@@ -52,7 +52,7 @@ int **init_simulation(int **game_map, int *n_cols, int *n_rows, int *population,
             }
           }
         }
-		break;
+        break;
       default:
         for (int x = 0; x < 3; x++) {
           for (int y = 0; y < 3; y++) {
@@ -103,54 +103,38 @@ int *get_random_coordinate(int x_lower, int x_upper, int y_lower, int y_upper) {
 int **simulate_universe(int *population, int **game_map, int *n_rows,
                         int *n_cols) {
   int neighbor = 0;
-  int current_generation[*n_cols][*n_rows];
+  int current_generation[*n_cols][*n_rows], generation_cell_count = 0;
+  Cell current_generation_cells[MAX_CELLS];
 
   for (int x = 0; x < *n_rows; x++) {
     for (int y = 0; y < *n_cols; y++) {
-      if (game_map[y][x] == 1)
-        current_generation[y][x] = 1;
-      else if (game_map[y][x] == 0) {
-        current_generation[y][x] = 0;
-      }
-    }
-  }
-
-  printf("----------------------------------------------------------\n");
-  for (int x = 0; x < *n_rows; x++) {
-    for (int y = 0; y < *n_cols; y++) {
-      printf("%d ", current_generation[y][x]);
-    }
-    printf("\n");
-  }
-
-  for (int x = 0; x < *n_rows; x++) {
-    for (int y = 0; y < *n_cols; y++) {
+      Cell cell;
       neighbor = get_neighbor_count(game_map, x, y);
       if (x == 1 && y == 0) {
         printf("Neighbors: %d\n", neighbor);
       }
 
-      if (neighbor == 3 && current_generation[y][x] == 0) {
-        current_generation[y][x] = 1;
-      } else if ((neighbor < 2 || neighbor > 3) &&
-                 current_generation[y][x] == 1) {
-        current_generation[y][x] = 0;
-      } else if ((neighbor == 2 || neighbor == 3) &&
-                 current_generation[y][x] == 1) {
-        current_generation[y][x] = 1;
+      if (neighbor == 3 && game_map[y][x] == 0) {
+        cell = (Cell){.x = x, .y = y, .alive = 1};
+      } else if ((neighbor < 2 || neighbor > 3) && game_map[y][x] == 1) {
+        cell = (Cell){.x = x, .y = y, .alive = 0};
+      } else if ((neighbor == 2 || neighbor == 3) && game_map[y][x] == 1) {
+        cell = (Cell){x = x, .y = y, .alive = 1};
       }
 
       neighbor = 0;
+      current_generation_cells[generation_cell_count] = cell;
+      generation_cell_count++;
     }
   }
 
-  for (int x = 0; x < *n_rows; x++) {
-    for (int y = 0; y < *n_cols; y++) {
-      if (current_generation[y][x] == 1)
-        game_map[y][x] = 1;
-      else if (current_generation[y][x] == 0) {
-        game_map[y][x] = 0;
-      }
+  for (int i = 0; i < generation_cell_count; i++) {
+    if (current_generation_cells[i].alive == 1) {
+      game_map[current_generation_cells[i].y][current_generation_cells[i].x] =
+          1;
+    } else if (current_generation_cells[i].alive == 0) {
+      game_map[current_generation_cells[i].y][current_generation_cells[i].x] =
+          0;
     }
   }
 
